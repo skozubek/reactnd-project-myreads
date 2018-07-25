b4fa39e59f3dbffc132cdbdea50ebdfb3f6b1776
Sergi <sergiusz.kozubek@gmail.com>
2018-07-25 20:02:05 +0200

    Set selected shelf on select item insted on option item

--- src/BookShelves.js
+++ src/BookShelves.js
@@ -24,14 +24,14 @@ class BookShelves extends Component {
               <div className="bookshelf-books">
                 <ol className="books-grid">
                 {showingBooks.filter((book) => book.shelf === 'currentlyReading').map((book) => (
-                  <li key={ book.id }>
+                  <li key={book.id}>
                     <div className="book">
                       <div className="book-top">
                         <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
                         <div className="book-shelf-changer">
-                          <select>
+                          <select defaultValue="currentlyReading">
                             <option value="move" disabled>Move to...</option>
-                            <option value="currentlyReading" selected>Currently Reading</option>
+                            <option value="currentlyReading">Currently Reading</option>
                             <option value="wantToRead">Want to Read</option>
                             <option value="read">Read</option>
                             <option value="none">None</option>
@@ -39,7 +39,7 @@ class BookShelves extends Component {
                         </div>
                       </div>
                       <div className="book-title">{book.title}. {book.subtitle}</div>
-                      <div className="book-authors">{book.authors.join()}</div>
+                      <div className="book-authors">{book.authors.join(', ')}</div>
                     </div>
                   </li>
                 ))}
@@ -55,10 +55,10 @@ class BookShelves extends Component {
                       <div className="book-top">
                         <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
                         <div className="book-shelf-changer">
-                          <select>
+                          <select defaultValue="wantToRead">
                             <option value="move" disabled>Move to...</option>
                             <option value="currentlyReading">Currently Reading</option>
-                            <option value="wantToRead" selected>Want to Read</option>
+                            <option value="wantToRead">Want to Read</option>
                             <option value="read">Read</option>
                             <option value="none">None</option>
                           </select>
@@ -81,11 +81,11 @@ class BookShelves extends Component {
                       <div className="book-top">
                         <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
                         <div className="book-shelf-changer">
-                          <select>
+                          <select defaultValue="read">
                             <option value="move" disabled>Move to...</option>
                             <option value="currentlyReading">Currently Reading</option>
                             <option value="wantToRead">Want to Read</option>
-                            <option value="read" selected>Read</option>
+                            <option value="read">Read</option>
                             <option value="none">None</option>
                           </select>
                         </div>
